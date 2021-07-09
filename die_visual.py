cdf839db10f3ae4ce3afc5e60d36f9f2c499a535
Ken V <ken.valverde@yahoo.com>
2021-07-09 13:14:02 -0500

    analyze results of rolling a D6

--- die_visual.py
+++ die_visual.py
@@ -5,8 +5,14 @@ dice = Die()
 
 # Make some rolls and store results in a list
 results = []
-for roll_num in range(100):
+for roll_num in range(1000):
     result = dice.roll()
     results.append(result)
 
-print(results)
+# Analyze the results
+frequencies = []
+for value in range(1, dice.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
